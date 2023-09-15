17d587a1b02f1af4d9fd86eccfb7d808802b4705
MikeMinheere <99067548@mydavinci.nl>
2023-09-15 11:05:54 +0200

    01 changed pupil to student

--- ELO/database/migrations/2014_10_12_000000_create_users_table.php
+++ ELO/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->integer('pupil_number');
+            $table->integer('student_number');
             $table->string('class_name');
             $table->string('password');
             $table->rememberToken();
