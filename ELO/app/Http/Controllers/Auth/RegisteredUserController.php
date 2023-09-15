a27be10c337a0cb09fc6fa036e65338c5489482e
MikeMinheere <99067548@mydavinci.nl>
2023-09-15 11:28:25 +0200

    01 studentNumber almost works

--- ELO/app/Http/Controllers/Auth/RegisteredUserController.php
+++ ELO/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,12 +34,14 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'student_number' => ['required', 'integer', 'unique:'.User::class]
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'student_number' =>$request->student_number
         ]);
 
         event(new Registered($user));
