654a48c87150f974b7df0f503cc8fe5915c8fa87
MikeMinheere <99067548@mydavinci.nl>
2023-09-29 09:30:46 +0200

    moving to 1 login

--- ELO/database/migrations/2014_10_12_000000_create_users_table.php
+++ ELO/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->integer('student_number');
             $table->string('class_name');
             $table->string('password');
+            $table->string('role');
+            $table->string('profilePicture');
             $table->rememberToken();
             $table->timestamps();
         });
