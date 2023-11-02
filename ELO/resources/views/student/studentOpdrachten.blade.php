c1cc59d493c6053dfdd825a7a140d861291b1f94
Matthew01234 <99067338@mydavinci.nl>
2023-11-02 12:22:33 +0100

    05 kleine changes

--- ELO/resources/views/student/studentOpdrachten.blade.php
+++ ELO/resources/views/student/studentOpdrachten.blade.php
@@ -5,14 +5,14 @@
 @section('content')
 
 <div class="container-button">
-  <a href="/studentOpdrachten" class="button-3 w-button">OPDRACHTEN</a>
+  <a href="/docentOpdrachtenCreate" class="button-3 w-button">opdracht aanmaken</a>
   <a href="#" class="button-3 w-button">ROOSTER</a>
 </div>
 
 @foreach ($opdrachten as $opdracht)
 <tr>
   
-<div style="width:300px; height:40px; background-color:318D7E; margin-bottom:20px; border-radius:10px">
+<div style="width:300px; height:70px; background-color:318D7E; margin-top:20px; margin-bottom:20px; border-radius:10px;text-align:center;">
 
 <td>{{$opdracht ->opdracht_naam}}</td>
 </div>
