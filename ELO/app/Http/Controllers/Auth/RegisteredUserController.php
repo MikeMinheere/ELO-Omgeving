32b81a86128d2c5b38dc156a45b3c03b11c915f6
MikeMinheere <99067548@mydavinci.nl>
2023-11-06 10:51:00 +0100

    fixed prefix field

--- ELO/app/Http/Controllers/Auth/RegisteredUserController.php
+++ ELO/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,7 +32,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'first_name' => ['required', 'string', 'max:255'],
-            'prefix' => ['string', 'max:255'],
+            'prefix' => ['nullable','string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
--- ELO/database/migrations/2014_10_12_000000_create_users_table.php
+++ ELO/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('first_name')->nullable();
-            $table->string("prefix")->nullable();
+            $table->string('prefix')->nullable();
             $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
