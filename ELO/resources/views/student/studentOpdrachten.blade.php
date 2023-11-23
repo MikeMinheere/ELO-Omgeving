0fd9781e1432f44b16a1c51ab737a96c3fd8e0aa
Matthew01234 <99067338@mydavinci.nl>
2023-11-23 09:54:50 +0100

    05-changes

--- ELO/resources/views/student/studentOpdrachten.blade.php
+++ ELO/resources/views/student/studentOpdrachten.blade.php
@@ -2,26 +2,26 @@
 
 <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
 
-@section('content')
+@section('content'i )
 
 @if(Auth::user()->role == "teacher")
     
 <div class="container-button">
-  <a href="/docentOpdrachtCreate" class="button-3 w-button">Opdracht aanmaken</a>
-  <a href="/docentDashboard" class="button-3 w-button">Dashboard</a>
+  <a href="/docentOpdrachtenCreate" class="button-3 w-button">Aanmaken</a>
+  <a href="/docentDashboard" class="button-3 w-button">🔙</a>
 </div>
 @endif
 
-<div class = "div-opdracht">
 @foreach ($opdrachten as $opdracht)
 <tr>
-  <button type="submit" class = "button-opdracht">
-    <td><h2>{{$opdracht ->opdracht_naam}}</h2></td>
-    <td>{{$opdracht ->opdracht_type}}</td>
-  </button>
+  
+<div style="width:300px; height:40px; background-color:318D7E; margin-bottom:20px; border-radius:10px position:center;">
+
+<td>{{$opdracht ->opdracht_naam}}</td>
+</div>
+
 </tr>
 
 @endforeach
-</div>
 
 @endsection
