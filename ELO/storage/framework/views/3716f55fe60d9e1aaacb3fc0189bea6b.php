458292bafe49134a1f137090e35380a3f67aab75
Hamza-Hammouti <99069686@mydavinci.nl>
2023-12-18 10:38:59 +0100

    homepage updated and cleaned up.

--- ELO/storage/framework/views/3716f55fe60d9e1aaacb3fc0189bea6b.php
+++ ELO/storage/framework/views/3716f55fe60d9e1aaacb3fc0189bea6b.php
@@ -1,28 +1,56 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Gabarito&display=swap" rel="stylesheet">
 
-<div style="width: 1440px; height: 2092px; position: relative; background: white; font-family: 'Gabarito';">
-  <div style="width: 1248px; height: 771px; left: 96px; top: 50px; position: absolute; background: linear-gradient(180deg, #318D7E 0%, #8CBE8D 100%); border-radius: 20px"></div>
-  <div style="width: 367px; height: 711px; left: 416px; top: 80px; position: absolute; background: white; border-radius: 20px"></div>
-  <div style="width: 367px; height: 711px; left: 870px; top: 80px; position: absolute; background: white; border-radius: 20px"></div>
-  <div style="width: 663.59px; height: 201.26px; left: 245.03px; top: 768.20px; position: absolute; transform: rotate(-90.18deg); transform-origin: 0 0; color: white; font-size: 48px; font-family: Inter; font-weight: 600; line-height: 62.40px; word-wrap: break-word">Engels Media & Techniek</div>
-    <img style="width: 147px; height: 144px; left: 130px; top: 63px; position: absolute" src="image/logo.png" />
-    <img style="width: 164px; height: 205px; left: 506px; top: 104px; position: absolute" src="image/manZittend.png" />
-    <img style="width: 134px; height: 219px; left: 987px; top: 104px; position: absolute" src="image/vrouwZittend.png" />
-  <div style="width: 220px; height: 235px; left: 481px; top: 405px; position: absolute; text-align: center; color: black; font-size: 14px; font-family: ABeeZee; font-weight: 400; word-wrap: break-word">"Time to shine, studenten! Jullie hebben de kracht in je om deze opdrachten te rocken en jullie talent te laten zien. Stel je doelen, neem een diepe adem, en ga ervoor. De wereld wacht op jullie briljante ideeën. 💪📚 #LetsDoThis"</div>
-  <div style="width: 220px; height: 235px; left: 939px; top: 431px; position: absolute; text-align: center; color: black; font-size: 14px; font-family: ABeeZee; font-weight: 400; word-wrap: break-word">Laten we samen de vonk van kennis ontsteken, docenten! Jullie zijn de gidsen die de toekomst vormgeven. Breng passie, creativiteit en inspiratie naar het klaslokaal. Laat jullie enthousiasme stralen en zie hoe jullie studenten groeien. 🌟📖 #OnderwijsPassie</div>
-  
-  <div style="padding-top: 19px; padding-bottom: 19px; padding-left: 65px; padding-right: 64px; left: 477px; top: 608px; position: absolute; background: #318D7E; border-radius: 8px; overflow: hidden; justify-content: center; align-items: center; display: inline-flex">
-     
-     <a  href="register" style="color: white; font-size: 20px; font-family: Inter; font-weight: 600; line-height: 26px; word-wrap: break-word">Registreer</a>
-  </div>
+<div style="width: 100%; height: 100%; position: relative; background: white; font-family: 'Gabarito';">
+    <div
+        style="width: 1707px; height: 821px; left: -8px; top: -8px; position: absolute; background: linear-gradient(180deg, #318D7E 0%, #8CBE8D 100%);">
+    </div>
+    <div style="width: 367px; height: 700px; left: 475px; top: 80px; position: absolute; background: white;">
+    </div>
+    <div style="width: 367px; height: 700px; left: 870px; top: 80px; position: absolute; background: white;">
+    </div>
+    <div
+        style="width: 663.59px; height: 201.26px; left: 245.03px; top: 735.20px; position: absolute; transform: rotate(-90.18deg); transform-origin: 0 0; color: white; font-size: 48px; font-family: Gabarito; font-weight: 600; line-height: 62.40px; word-wrap: break-word">
+        Engels Media & Techniek</div>
+    <img style="width: 147px;height: 144px;left: 207px;top: 43px;position: absolute" src="image/logo.png" />
+    <img style="width: 164px; height: 205px; left: 587px; top: 104px; position: absolute" src="image/manZittend.png" />
+    <img style="width: 134px; height: 219px; left: 987px; top: 104px; position: absolute"
+        src="image/vrouwZittend.png" />
+    <div
+        style="width: 220px; height: 235px; left: 552px; top: 405px; position: absolute; text-align: center; color: black; font-size: 14px; font-family: Gabarito; font-weight: 400; word-wrap: break-word">
+        "Time to shine, studenten! Jullie hebben de kracht in je om deze opdrachten te rocken en jullie talent te laten
+        zien. Stel je doelen, neem een diepe adem, en ga ervoor. De wereld wacht op jullie briljante ideeën. 💪📚
+        #LetsDoThis"</div>
+    <div
+        style="width: 220px; height: 235px; left: 942px; top: 405px; position: absolute; text-align: center; color: black; font-size: 14px; font-family: Gabarito; font-weight: 400; word-wrap: break-word">
+        Laten we samen de vonk van kennis ontsteken, docenten! Jullie zijn de gidsen die de toekomst vormgeven. Breng
+        passie, creativiteit en inspiratie naar het klaslokaal. Laat jullie enthousiasme stralen en zie hoe jullie
+        studenten groeien. 🌟📖 #OnderwijsPassie</div>
 
-  <div style="width: 227px; padding-top: 19px; padding-bottom: 19px; left: 477px; top: 704px; position: absolute; background: #318D7E; border-radius: 8px; overflow: hidden; justify-content: center; align-items: center; display: inline-flex">
-  <a  href="login" style="color: white; font-size: 20px; font-family: Inter; font-weight: 600; line-height: 26px; word-wrap: break-word">Login</a>
-  </div>
 
-  <div style="width: 227px; padding-top: 19px; padding-bottom: 19px; left: 940px; top: 704px; position: absolute; background: #318D7E; border-radius: 8px; overflow: hidden; justify-content: center; align-items: center; display: inline-flex">
-  <a  href="login" style="color: white; font-size: 20px; font-family: Inter; font-weight: 600; line-height: 26px; word-wrap: break-word">Login</a>
-  </div>
+    <!--<div
+        style="width: 227px; padding-top: 19px; padding-bottom: 19px; left: 545px; top: 598px; position: absolute; background: #318D7E; border-radius: 8px; overflow: hidden; justify-content: center; align-items: center; display: inline-flex">
+        <a href="login"
+            style="color: white; font-size: 20px; font-family: Gabarito; font-weight: 600; line-height: 26px; text-decoration: none; word-wrap: break-word">Login</a>
+    </div>-->
 
-  <div style="width: 263px; height: 93px; left: 456px; top: 346px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Changa One; font-weight: 400; word-wrap: break-word">Welkom Studenten</div>
-  <div style="width: 263px; height: 93px; left: 922px; top: 346px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Changa One; font-weight: 400; word-wrap: break-word">Welkom Docenten</div>
+    <a href="login"
+        style="width: 227px; padding-top: 19px; padding-bottom: 19px; left: 545px; top: 598px; position: absolute; background: #318D7E; border-radius: 8px; overflow: hidden; justify-content: center; align-items: center; display: inline-flex; color: white; font-size: 20px; font-family: Gabarito; font-weight: 600; line-height: 26px; text-decoration: none; word-wrap: break-word">Login
+    </a>
+    
+    <a href="login"
+        style="width: 227px; padding-top: 19px; padding-bottom: 19px; left: 940px; top: 598px; position: absolute; background: #318D7E; border-radius: 8px; overflow: hidden; justify-content: center; align-items: center; display: inline-flex; color: white; font-size: 20px; font-family: Gabarito; font-weight: 600; line-height: 26px; text-decoration: none; word-wrap: break-word">Login
+    </a>
+
+    <a href="register"
+        style="padding-top: 8px; padding-bottom: 10px; padding-left: 53px; padding-right: 53px; left: 573px; top: 675px; position: absolute; background: #318D7E; border-radius: 8px; overflow: hidden; justify-content: center; align-items: center; display: inline-flex; color: white; font-size: 16px; font-family: Gabarito; font-weight: 600; line-height: 26px; text-decoration: none; word-wrap: break-word">Registreer
+    </a>
+
+    <div
+        style="width: 263px; height: 93px; left: 531px; top: 346px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Gabarito; font-weight: 400; word-wrap: break-word">
+        Welkom Studenten</div>
+    <div
+        style="width: 263px; height: 93px; left: 922px; top: 346px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Gabarito; font-weight: 400; word-wrap: break-word">
+        Welkom Docenten</div>
 </div><?php /**PATH C:\laragon\www\ELO-Omgeving\ELO\resources\views/welcome.blade.php ENDPATH**/ ?>
